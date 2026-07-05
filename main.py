7de73eee0503a9b26d03111d5bc9987cbcc8d100
Juan Pablo Lenain <juanpablolenain222@gmail.com>
2026-07-04 22:54:19 -0300

    Actualiza main.py con menú base y opciones 1 y 2

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 from altas import registrar_paciente
 from solicitar_turno import solicitar_turno
+
 pacientes = {}
 turnos = []
 especialidades = {
-    "Clinica medica": {
-        "cupo_maximo": 5,
-        "turnos_asignados": 0
-    },
-    "Pediatria": {
-        "cupo_maximo": 4,
-        "turnos_asignados": 0
-    },
-    "Traumatologia": {
-        "cupo_maximo": 3,
-        "turnos_asignados": 0
-    },
-    "Cardiologia": {
-        "cupo_maximo": 1,
-        "turnos_asignados": 0
-    },
-    "Guardia": {
-        "cupo_maximo": 10,
-        "turnos_asignados": 0
-    }
+    "Clinica medica":  {"cupo_maximo": 5, "turnos_asignados": 0},
+    "Pediatria":       {"cupo_maximo": 4, "turnos_asignados": 0},
+    "Traumatologia":   {"cupo_maximo": 3, "turnos_asignados": 0},
+    "Cardiologia":     {"cupo_maximo": 1, "turnos_asignados": 0},
+    "Guardia":         {"cupo_maximo": 3, "turnos_asignados": 0}
 }
 
 
@@ -30,15 +16,15 @@ def mostrar_menu():
     print("\n--- SISTEMA DE TURNOS HOSPITALARIOS ---")
     print("1. Registrar paciente")
     print("2. Pedir un turno")
-    print("4. Ver turnos pendientes")
-    print("5. Atender siguiente turno")
-    print("6. Ver estadísticas")
-    print("7. Salir")
+    print("3. Ver turnos pendientes")
+    print("4. Atender siguiente turno")
+    print("5. Ver estadísticas")
+    print("6. Salir")
 
 
 def main():
     opcion = ""
-    while opcion != 7:
+    while opcion != 6:
         mostrar_menu()
         try:
             opcion = int(input("Seleccione una opción: "))
@@ -51,9 +37,15 @@ def main():
             case 2:
                 solicitar_turno(pacientes, turnos, especialidades)
             case 3:
+                print("(en desarrollo)")
+            case 4:
+                print("(en desarrollo)")
+            case 5:
+                print("(en desarrollo)")
+            case 6:
                 print("Saliendo del sistema...")
             case _:
                 print("Opción inválida.")
-    print(turnos)
-main()
 
+
+main()
