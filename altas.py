97d0c8713f3ef86072da858a0a175fac9a33a06e
Joshua David Lezcano Genes <joshualezcano8@gmail.com>
2026-07-04 21:52:45 -0300

    altas.py: registro de pacientes con validaciones

--- altas.py
+++ altas.py
@@ -1,7 +1,8 @@
 from validaciones import validar_dni, validar_nombre, validar_edad, validar_obra_social
 
+
 def registrar_paciente(pacientes):
-    print("\n--- Registro de paciente ---")
+    print("\n--- Registro de Paciente ---")
     while True:
         dni = input("Ingrese DNI: ").strip()
         if validar_dni(dni):
@@ -10,15 +11,15 @@ def registrar_paciente(pacientes):
         print("Error: ya existe un paciente registrado con ese DNI.")
         return
     while True:
-        nombre = input("Ingrese nombre y apellido: ").strip()
+        nombre = input("Ingrese Nombre y Apellido: ").strip()
         if validar_nombre(nombre):
             break
     while True:
-        edad = input("Ingrese edad: ").strip()
+        edad = input("Ingrese Edad: ").strip()
         if validar_edad(edad):
             break
     while True:
-        obra_social = input("Ingrese obra social o escriba Particular: ").strip().upper()
+        obra_social = input("Ingrese Obra Social o escriba Particular: ").strip().upper()
         if validar_obra_social(obra_social):
             break
     pacientes[dni] = {
